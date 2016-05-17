16d7e99afd4b976c41365f8f91d13c552a124eab
lvyanlong <longlong080729@gmail.com>
2016-05-17 16:26:11 +0800

    添加输入内容显示到HTML

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var content = 11111;
 console.log("searchbar content = %s", content);
 function onclickBtn(){
 	content = document.getElementById("searchbar").value;
+	var para=document.createElement("p");
+	var node=document.createTextNode(content);
+	para.appendChild(node);
+
+	var element=document.getElementById("insert");
+	element.appendChild(para);
 	console.log("searchbar content = %s", content);
 }
